e0bb0a55d02d9dcc4303157c5e091334fa83127f
Nicolas <nicolas.guruphat@etu.univ-lyon1.fr>
2021-09-24 23:00:54 +0200

    Boucle for en trop enlevée Ajout de commentaire

--- classipycator.py
+++ classipycator.py
@@ -1,31 +1,27 @@
 import os
 path=os.getcwd()
 repositoryPath = "C:\\Users\\nicog\\OneDrive\\Bureau\\Classification"
-extension=[element.lower() for element in os.listdir(windowPath)]
-print(extension)
+extension=[element.lower() for element in os.listdir(repositoryPath)]
 choice=""
-while(choice!="yes"):
-	choice=input("Do you want to classify this path :\n"+path+"\n yes/no\n")
-for element in os.listdir(path):
+while(choice!="yes" and choice != "no"):
+	print(choice)
+	choice=input("Do you want to classify this path :\n"+path+"\nyes/no\n")
+if choice =="yes":
+	for element in os.listdir(path):
+	#parcours tout les éléments présents dans le dossier où se trouve classiPycator
+		if element != "classipycator.py":
+			currentFileExtension= element[(element.rfind(".")+1):]
 
-	if element != "classipycator.py":
-		currentFileExtension= element[(element.rfind(".")+1):]
+			if currentFileExtension.lower() not in extension:
+				extension.append(currentFileExtension)
+				os.mkdir(repositoryPath+"\\"+currentFileExtension)
 
-		if currentFileExtension.lower() not in extension:
-			extension.append(currentFileExtension)
-			os.mkdir(windowPath+"\\"+currentFileExtension)
-
-		for ext in extension:
-
-			if element[-len(ext):] == ext:
-
-				try :
-					os.rename("{}\\{}".format(path,element), "{}\\{}\\{}".format(windowPath,ext,element))
-					print("{}\\{}".format(path,element)," move ", "({})".format(ext))
-				
-				except Exception as e:
+			try :
+				os.rename("{}\\{}".format(path,element), "{}\\{}\\{}".format(repositoryPath,currentFileExtension,element))
+				print("{}\\{}".format(path,element)," move ", "({})".format(currentFileExtension))
+		
+			except Exception as e:
 					print("Error : file {} has not been moved".format(element))
 					print("Error : ",e )
-				
-				break
+	
 fermer=input("Press enter to close...")
