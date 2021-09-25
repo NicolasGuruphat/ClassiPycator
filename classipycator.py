bd9cdf02d715ec3df5104695c83151a87c3293a7
Nicolas <nicolas.guruphat@etu.univ-lyon1.fr>
2021-09-25 13:04:43 +0200

    Ajout menu de séléction du dossier à traiter (dossier dans lequel est le .py reste le dossier de base)

--- classipycator.py
+++ classipycator.py
@@ -1,11 +1,18 @@
 import os
+from tkinter import Tk, filedialog
 path=os.getcwd()
 repositoryPath = "C:\\Users\\nicog\\OneDrive\\Bureau\\Classification"
 extension=[element.lower() for element in os.listdir(repositoryPath)]
 choice=""
 while(choice!="yes" and choice != "no"):
-	print(choice)
 	choice=input("Do you want to classify this path :\n"+path+"\nyes/no\n")
+	if choice=="no":
+		root = Tk()
+		root.withdraw()
+		root.attributes('-topmost', True)
+		open_file = filedialog.askdirectory()
+		path = open_file
+		choice="repeat"
 if choice =="yes":
 	for element in os.listdir(path):
 	#parcours tout les éléments présents dans le dossier où se trouve classiPycator
@@ -18,7 +25,7 @@ if choice =="yes":
 
 			try :
 				os.rename("{}\\{}".format(path,element), "{}\\{}\\{}".format(repositoryPath,currentFileExtension,element))
-				print("{}\\{}".format(path,element)," move ", "({})".format(currentFileExtension))
+				print("{}\\{}".format(path,element)," move ", "({})".format(repositoryPath + "\\"+currentFileExtension))
 		
 			except Exception as e:
 					print("Error : file {} has not been moved".format(element))
