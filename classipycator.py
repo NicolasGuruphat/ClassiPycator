5e7a1102b07dd151323d04012963ffa7efe9b0d4
Nicolas Guruphat <n.guruphat@adequation-france.com>
2022-12-28 13:42:11 +0100

    Format document

--- classipycator.py
+++ classipycator.py
@@ -1,36 +1,37 @@
 import os
 from tkinter import Tk, filedialog
-path=os.getcwd()
+path = os.getcwd()
 repositoryPath = "C:\\Users\\nicog\\OneDrive\\Bureau\\Classification"
-extension=[element.lower() for element in os.listdir(repositoryPath)]
-choice=""
-while(choice!="yes" and choice != "no"):
-	choice=input("Do you want to classify this path :\n"+path+"\nyes/no\n")
-	if choice=="no":
-		root = Tk()
-		root.withdraw()
-		root.attributes('-topmost', True)
-		open_file = filedialog.askdirectory()
-		path = open_file
-		choice="repeat"
-		
-if choice =="yes":
-	for element in os.listdir(path):
-	#parcours tout les éléments présents dans le dossier où se trouve classiPycator
-		if element != __file__[__file__.rfind("\\")+1:]:
-			currentFileExtension= element[(element.rfind(".")+1):]
+extension = [element.lower() for element in os.listdir(repositoryPath)]
+choice = ""
+while (choice != "yes" and choice != "no"):
+    choice = input("Do you want to classify this path :\n"+path+"\nyes/no\n")
+    if choice == "no":
+        root = Tk()
+        root.withdraw()
+        root.attributes('-topmost', True)
+        open_file = filedialog.askdirectory()
+        path = open_file
+        choice = "repeat"
 
-			if currentFileExtension.lower() not in extension:
-				extension.append(currentFileExtension)
-				os.mkdir(repositoryPath+"\\"+currentFileExtension)
+if choice == "yes":
+    for element in os.listdir(path):
+        # parcours tout les éléments présents dans le dossier où se trouve classiPycator
+        if element != __file__[__file__.rfind("\\")+1:]:
+            currentFileExtension = element[(element.rfind(".")+1):]
 
-			try :
-				os.rename("{}\\{}".format(path,element), 
-					      "{}\\{}\\{}".format(repositoryPath,currentFileExtension,element))
-				print("{}\\{} -> {}\\{}\\{}".format(path,element, repositoryPath,currentFileExtension,element))
-		
-			except Exception as e:
-					print("Error : file {} has not been moved".format(element))
-					print("Error : ",e )
-	
-close=input("Press enter to close...")
+            if currentFileExtension.lower() not in extension:
+                extension.append(currentFileExtension)
+                os.mkdir(repositoryPath+"\\"+currentFileExtension)
+
+            try:
+                os.rename("{}\\{}".format(path, element),
+                          "{}\\{}\\{}".format(repositoryPath, currentFileExtension, element))
+                print("{}\\{} -> {}\\{}\\{}".format(path, element,
+                      repositoryPath, currentFileExtension, element))
+
+            except Exception as e:
+                print("Error : file {} has not been moved".format(element))
+                print("Error : ", e)
+
+close = input("Press enter to close...")
