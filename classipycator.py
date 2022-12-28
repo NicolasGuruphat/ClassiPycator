c13d19f2e376c5edb08356ff62a9b377a88f65cd
Nicolas Guruphat <n.guruphat@adequation-france.com>
2022-12-28 13:43:16 +0100

    Rename

--- classipycator.py
+++ classipycator.py
@@ -1,8 +1,8 @@
 import os
 from tkinter import Tk, filedialog
 path = os.getcwd()
-repositoryPath = "C:\\Users\\nicog\\OneDrive\\Bureau\\Classification"
-extension = [element.lower() for element in os.listdir(repositoryPath)]
+folderPath = "C:\\Users\\nicog\\OneDrive\\Bureau\\Classification"
+extension = [element.lower() for element in os.listdir(folderPath)]
 choice = ""
 while (choice != "yes" and choice != "no"):
     choice = input("Do you want to classify this path :\n"+path+"\nyes/no\n")
@@ -22,13 +22,13 @@ if choice == "yes":
 
             if currentFileExtension.lower() not in extension:
                 extension.append(currentFileExtension)
-                os.mkdir(repositoryPath+"\\"+currentFileExtension)
+                os.mkdir(folderPath+"\\"+currentFileExtension)
 
             try:
                 os.rename("{}\\{}".format(path, element),
-                          "{}\\{}\\{}".format(repositoryPath, currentFileExtension, element))
+                          "{}\\{}\\{}".format(folderPath, currentFileExtension, element))
                 print("{}\\{} -> {}\\{}\\{}".format(path, element,
-                      repositoryPath, currentFileExtension, element))
+                      folderPath, currentFileExtension, element))
 
             except Exception as e:
                 print("Error : file {} has not been moved".format(element))
